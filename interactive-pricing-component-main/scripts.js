b02994a0e9ab61d3811c915cc95901faa058146e
Gabriel Freitas <gabrielfreitas.ilustra@gmail.com>
2023-03-28 13:16:20 -0300

    grabbin elements & creating data

--- interactive-pricing-component-main/scripts.js
+++ interactive-pricing-component-main/scripts.js
@@ -1,1 +1,45 @@
-console.log("hellow")
+const viewsContainer = document.querySelector("span.pricing-card__view")
+const rangeInput = document.querySelector("input.pricing-card__views-range")
+const constContainer = document.querySelector("span.pricing-card__price")
+const priceFreqContainer = document.querySelector("span.pricing-card__billing-frequency")
+const frequencyToggle = document.querySelector("input#billing-freq")
+const form = document.querySelector('form.pricing-card')
+
+console.log({
+  rangeInput,
+  viewsContainer,
+  constContainer,
+  priceFreqContainer,
+  frequencyToggle,
+  form
+})
+
+const VIEWS_DATA = [
+  {
+    pageViews: '10k',
+    monthlyCost: 8,
+    leftPercentage: 0
+  },
+  {
+    pageViews: '50k',
+    monthlyCost: 12,
+    leftPercentage: 25
+  },
+  {
+    pageViews: '100k',
+    monthlyCost: 16,
+    leftPercentage: 50
+  },
+  {
+    pageViews: '500k',
+    monthlyCost: 25,
+    leftPercentage: 75
+  },
+  {
+    pageViews: '10M',
+    monthlyCost: 36,
+    leftPercentage: 100
+  },
+]
+
+form.addEventListener('submit', (e) => e.preventDefault())
